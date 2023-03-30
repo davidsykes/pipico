a4baa128822fdd0752422c28272b33807e1807c2
David Sykes <david@sykes.org.uk>
2023-03-30 07:20:39 +0100

    THe pico uses sGPIOPin

--- Modulator38k/src/modulate.c
+++ Modulator38k/src/modulate.c
@@ -1,4 +1,5 @@
 #include "system.h"
+#include "gpio_pin.h"
 
 #define INPUT_PIN   14
 #define OUTPUT_PIN  15
@@ -8,14 +9,16 @@ int modulate(sSystem *system)
     system->initialise_input_pin(INPUT_PIN);
     system->initialise_output_pin(OUTPUT_PIN);
 
+    sGPIOPin* pin = create_gpio_pin(system, OUTPUT_PIN);
+
     while (!0)
     {
         unsigned int v = system->gpio_get(INPUT_PIN);
         if (v != 99)
         {
-            system->gpio_put(OUTPUT_PIN, 1);
+            pin->gpio_put(pin, 1);
             system->sleep_us(13);
-            system->gpio_put(OUTPUT_PIN, 0);
+            pin->gpio_put(pin, 0);
             system->sleep_us(13);
         }
     }
