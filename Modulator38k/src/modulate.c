e6a70c06f68a98bbca755b5f484dbcf4691bceac
David Sykes <david@sykes.org.uk>
2023-03-30 08:09:40 +0100

    Wire up the pico modulator pins

--- Modulator38k/src/modulate.c
+++ Modulator38k/src/modulate.c
@@ -1,24 +1,25 @@
 #include "system.h"
 #include "gpio_pin.h"
 
-#define INPUT_PIN   14
-#define OUTPUT_PIN  15
+#define INPUT_PIN   16
+#define OUTPUT_PIN  22
 
 int modulate(sSystem *system)
 {
     system->initialise_input_pin(INPUT_PIN);
     system->initialise_output_pin(OUTPUT_PIN);
 
-    sGPIOPin* pin = create_gpio_pin(system, OUTPUT_PIN);
+    sGPIOPin* ipin = create_gpio_pin(system, INPUT_PIN);
+    sGPIOPin* opin = create_gpio_pin(system, OUTPUT_PIN);
 
     while (!0)
     {
-        unsigned int v = system->gpio_get(INPUT_PIN);
-        if (v != 99)
+        unsigned int v = ipin->gpio_get(ipin);
+        if (v != 0)
         {
-            pin->gpio_put(pin, 1);
+            opin->gpio_put(opin, 1);
             system->sleep_us(13);
-            pin->gpio_put(pin, 0);
+            opin->gpio_put(opin, 0);
             system->sleep_us(13);
         }
     }
