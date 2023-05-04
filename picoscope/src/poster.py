2a7600b39f1e4a505d0608a198793540752633cb
David Sykes <david@sykes.org.uk>
2023-05-04 07:12:12 +0100

    Scope poster sets up url correctly

--- picoscope/src/poster.py
+++ picoscope/src/poster.py
@@ -3,11 +3,13 @@ import json
 class Poster:
     def __init__(self, system, server_ip):
         self.system = system
-        self.server_ip = server_ip
+        self.server_url = self.make_server_url(server_ip)
+
+    def make_server_url(self, server_ip):
+        return ''.join(['http://', server_ip, '/scope'])
 
     def post_data(self, data):
-        url = self.server_ip + '/trace'
         hex_data = data.hex()
         body = {'title': 'Yo', 'trace' : hex_data}
         body_json = json.dumps(body)
-        self.system.put_json(url, body_json)
+        self.system.put_json(self.server_url, body_json)
--- picoscope/tests/test_poster.py
+++ picoscope/tests/test_poster.py
@@ -10,10 +10,14 @@ class MockSystem:
 class TestPoster:
     def setup_method(self, test_method):
         self.mock_system = MockSystem()
-        self.poster = Poster(self.mock_system, 'server url')
+        self.poster = Poster(self.mock_system, 'server ip')
+
+    def test_post_data_posts_to_http_url(self):
+        self.poster.post_data(b'Hello World')
+
+        assert(self.mock_system.put_url == 'http://server ip/scope')
 
     def test_post_data_posts_to_the_server_in_json(self):
         self.poster.post_data(b'Hello World')
 
-        assert(self.mock_system.put_url == 'server url/trace')
         assert(self.mock_system.put_data == '{"title": "Yo", "trace": "48656c6c6f20576f726c64"}')
