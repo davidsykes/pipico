a140ca97ac9678f43988d1c59d4644b08db122ec
David Sykes <david@sykes.org.uk>
2023-05-18 07:32:50 +0100

    Trace apture captures values at an acceptable speed

--- picoscope/tracecapture/src/main.c
+++ picoscope/tracecapture/src/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "pico/stdlib.h"
+#include "pico/malloc.h"
 #include "hardware/uart.h"
 #include "pico/cyw43_arch.h"
 
@@ -27,11 +29,33 @@ void send_uint64(uint64_t l)
     send_bytes((unsigned char*)&l, 8);
 }
 
+void send_int(int i)
+{
+    send_bytes((unsigned char*)&i, 4);
+}
+
+void send_char(unsigned char i)
+{
+    send_bytes(&i, 1);
+}
+
 void send_data(uint64_t time, unsigned char pins)
 {
     send_uint64(time);
     send_bytes(&pins, 1);
 }
+
+void send_all_data(uint64_t* times, unsigned char *values, int count)
+{
+    send_int(0x12345678);
+    send_int(count);
+    for (int i = 0 ; i < count ; i++)
+    {
+        send_uint64(times[i]);
+        send_char(values[i]);
+    }
+   send_int(0x87654321);
+ }
 
 int main()
 {
@@ -44,23 +68,27 @@ int main()
     gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
     gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
 
-    uart_putc_raw(UART_ID, 'A');
-    uart_putc(UART_ID, 'B');
-    uart_puts(UART_ID, " Hello, UART!\n");
+    uart_puts(UART_ID, "Trace capture starting");
 
     for (int p = 8 ; p <= 15 ; p++)
     {
         gpio_init(p);
         gpio_set_dir(p, GPIO_IN);
         gpio_pull_up(p);
-    }    
+    }
 
-    char current_pins = 42;
+    int trace_count = 50;
+    uint64_t *trace_times = malloc(trace_count * sizeof(uint64_t));
+    unsigned char* trace_values = malloc(trace_count * sizeof(unsigned char));
 
     while(1)
     {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-        uart_puts(UART_ID, "Waiting");
+
+        char current_pins = get_pins();
+        trace_times[0] = 0;
+        trace_values[0] = current_pins;
+        int current_trace = 1;
 
         char new_pins;
         do
@@ -72,10 +100,10 @@ int main()
         uint64_t start_time = time_us_64();
         uint64_t current_time = start_time;
         uint64_t end_time = start_time + 1000000;
-        send_uint64(0x0102030405060708);
-        send_uint64(0x01020304);
-        send_uint64(0x0102030405060708);
-        send_data(current_time-start_time, current_pins);
+        //send_uint64(0x0102030405060708);
+        //send_uint64(0x01020304);
+        //send_uint64(0x0102030405060708);
+        //send_data(current_time-start_time, current_pins);
 
         current_pins = new_pins;
         do
@@ -85,12 +113,18 @@ int main()
             //if (new_pins != current_pins)
             {
                 current_pins = new_pins;
-                send_data(current_time-start_time, current_pins);
+                //send_data(current_time-start_time, current_pins);
+                trace_times[current_trace] = current_time-start_time;
+                trace_values[current_trace] = current_pins;
+                current_trace++;
             }
 
-        } while (current_time < end_time);
+        }
+        while (current_time < end_time && current_trace < trace_count);
+
+        send_all_data(trace_times, trace_values, current_trace);
         
-        send_uint64(0x0807060504030201);
-        uart_puts(UART_ID, "Stop");
+        //send_uint64(0x0807060504030201);
+        //uart_puts(UART_ID, "Stop");
     }
 }
