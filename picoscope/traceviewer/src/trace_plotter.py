dc971c8d3a270d7440c472c6623c333ebe41ca8d
David Sykes <david@sykes.org.uk>
2023-05-30 07:10:02 +0100

    A trace is plotted

--- picoscope/traceviewer/src/trace_plotter.py
+++ picoscope/traceviewer/src/trace_plotter.py
@@ -1,12 +1,20 @@
+import matplotlib.pyplot as plt
+
 class TracePlotter:
     def __init__(self, trace_directory, plot_system):
         self._trace_directory = trace_directory
         self._plot_system = plot_system
 
     def plot_traces(self, list_of_traces_to_plot):
+        fig, ax = plt.subplots()
         for trace_number in list_of_traces_to_plot:
             trace_data = self._trace_directory.get_trace_data_by_number(trace_number)
             sample_count = len(trace_data)
             print('Sample count:', sample_count)
+            x = []
+            y = []
             for s in trace_data:
-                print(s)
+                x.append(s['time'])
+                y.append(s['value'])
+            ax.plot(x, y)
+        plt.show()
