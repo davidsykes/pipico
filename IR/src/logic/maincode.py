5727d96bbc67a596a3f917a3ae040d1f426119f6
David Sykes <david@sykes.org.uk>
2023-06-05 07:26:56 +0100

    IR project default to a transmitter

--- IR/src/logic/maincode.py
+++ IR/src/logic/maincode.py
@@ -5,7 +5,7 @@ from configuration_retriever import ConfigurationRetriever
 
 IR_RECEIVE_PIN = 15
 IR_TRANSMIT_PIN = 14
-ServerUrl = 'http://192.168.1.87:5000'
+ServerUrl = 'http://192.168.1.142:5001'
 
 class MainCode:
     def maincode(self, system):
@@ -16,7 +16,12 @@ class MainCode:
         self.flasher.set_status(1)
         configuration = ConfigurationRetriever(self.service_access).retrieve_configuration()
 
-        if configuration == 'irtransmitter':
+        if configuration == 'irrecorder':
+            self.service_access.log('IR recorder')
+            from ir_recorder import IRRecorder
+            recorder = IRRecorder(system, self.service_access, IR_RECEIVE_PIN)
+            recorder.record(configuration_switches.dump_raw_ir_codes)
+        else:
             self.service_access.log('IR Transmitter')
             from controller import Controller
             ir_output = self.system.make_output_pin(IR_TRANSMIT_PIN)
@@ -24,11 +29,6 @@ class MainCode:
                         self.service_access,
                         ir_output)
             controller.control()
-        elif configuration == 'irrecorder':
-            self.service_access.log('IR recorder')
-            from ir_recorder import IRRecorder
-            recorder = IRRecorder(system, self.service_access, IR_RECEIVE_PIN)
-            recorder.record(configuration_switches.dump_raw_ir_codes)
 
     def set_up_flasher(self):
         self.flasher = Flasher(self.system)
