639a008b3ebe6333585b851242428248f8e077dd
David Sykes <david@sykes.org.uk>
2024-05-19 07:35:19 +0100

    Flash appears to be being written to

--- hotspot/hotspot/flash_program.c
+++ hotspot/hotspot/flash_program.c
@@ -9,18 +9,12 @@
 
 #include "pico/stdlib.h"
 #include "hardware/flash.h"
-
-
-int try_out_flash()
-{
-    printf("Generated random data:\n");
-}
-
+#include "hardware/sync.h"
 
 
 // We're going to erase and reprogram a region 256k from the start of flash.
 // Once done, we can access this at XIP_BASE + 256k.
-#define FLASH_TARGET_OFFSET (256 * 1024)
+#define FLASH_TARGET_OFFSET (1256 * 1024)
 
 const uint8_t *flash_target_contents = (const uint8_t *) (XIP_BASE + FLASH_TARGET_OFFSET);
 
@@ -34,8 +28,18 @@ void print_buf(const uint8_t *buf, size_t len) {
     }
 }
 
+// uint storage_get_flash_capacity() {
+//     uint8_t txbuf[STORAGE_CMD_TOTAL_BYTES] = {0x9f};
+//     uint8_t rxbuf[STORAGE_CMD_TOTAL_BYTES] = {0};
+//     flash_do_cmd(txbuf, rxbuf, STORAGE_CMD_TOTAL_BYTES);
+
+//     return 1 << rxbuf[3];
+// }
+
 int flash_main() {
-    stdio_init_all();
+    //printf("Flash size: %d\n", storage_get_flash_capacity());
+    printf("Flash page size: %d\n", FLASH_PAGE_SIZE);
+
     uint8_t random_data[FLASH_PAGE_SIZE];
     for (int i = 0; i < FLASH_PAGE_SIZE; ++i)
         random_data[i] = rand() >> 16;
@@ -45,12 +49,16 @@ int flash_main() {
 
     // Note that a whole number of sectors must be erased at a time.
     printf("\nErasing target region...\n");
+    uint32_t interrupts = save_and_disable_interrupts();
     flash_range_erase(FLASH_TARGET_OFFSET, FLASH_SECTOR_SIZE);
+    restore_interrupts(interrupts);
     printf("Done. Read back target region:\n");
     print_buf(flash_target_contents, FLASH_PAGE_SIZE);
 
     printf("\nProgramming target region...\n");
+    interrupts = save_and_disable_interrupts();
     flash_range_program(FLASH_TARGET_OFFSET, random_data, FLASH_PAGE_SIZE);
+    restore_interrupts(interrupts);
     printf("Done. Read back target region:\n");
     print_buf(flash_target_contents, FLASH_PAGE_SIZE);
 
@@ -63,4 +71,10 @@ int flash_main() {
         printf("Programming failed!\n");
     else
         printf("Programming successful!\n");
+}
+
+
+int try_out_flash()
+{
+    flash_main();
 }
