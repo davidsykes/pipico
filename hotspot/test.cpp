0504859d9c7c3884757de0ef35f95b8e9d138180
David Sykes <david@sykes.org.uk>
2024-05-23 07:05:16 +0100

    Setting up the connecting logic

--- hotspot/test.cpp
+++ hotspot/test.cpp
@@ -3,14 +3,25 @@
 #include "configurations/null_configuration.h"
 #include "configurations/led_configuration.h"
 #include "configurations/hotspot_configuration.h"
-//#include "logic/input_form/html_renderer.h"
 
 int main()
 {
+   SYSTEM_INTERFACE_T systemInterface;
+   SetUpSystemInterface(&systemInterface);
+   
+   WifiConnector wifiConnector(&systemInteface);
+   if (wifiConnector.CredentialsAreValid())
+   {
+      if (wifiConnector.ConnectToWifi())
+      {
+         DEBUG_printf("Connected");
+         return;
+      }
+   }
 
    //Configuration *config = new NullConfiguration();
    //Configuration *config = new LedConfiguration();
-   Configuration *config = new HotSpotConfiguration("Pico Test Unit");
+   Configuration *config = new HotSpotConfiguration("Pico Test Unit", wifiConnector.GetErrorMessage());
 
    connect_to_wifi(config);
 }
