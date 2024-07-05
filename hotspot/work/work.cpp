79a9750d8a07162e65b1936fb0054feae74b7b00
David Sykes <david@sykes.org.uk>
2024-07-05 07:04:42 +0100

    Investigating packet receipts

--- hotspot/work/work.cpp
+++ hotspot/work/work.cpp
@@ -8,7 +8,7 @@
 #define WIFI_SSID "a907"
 #define WIFI_PASSWORD "?thisistheWIFIyouhavebeenlookingfor1398"
 #define TEST_TCP_SERVER_IP "192.168.1.87"
-#define TCP_PORT 5001
+#define TCP_PORT 5000
 #define BUFFER_LENGTH   2048
 
 const char* get_message(void *data_object)
@@ -19,7 +19,7 @@ const char* get_message(void *data_object)
 
 void process_data(void *data_object, const char *data)
 {
-    printf("Received Packet\n%s\n", data);
+    printf("Received Packet\n'%s'-------\n", data);
     Worker* worker = (Worker*) data_object;
     printf("Worker %d\n", worker->number());
 }
@@ -40,7 +40,7 @@ int do_work(IHardwareInterface& hwif)
     uint64_t t = hwif.wait_value(15, 1-value, 10000000);
     printf("Change %d\n", (int)t);
 
-    PicoScope scope = PicoScope(hwif);
+    PicoScope scope = PicoScope(hwif, 100000);
     PicoScopeTrace trace = scope.FetchTrace(15);
     printf("Trace %s\n", trace.gethtml().c_str());
 
@@ -54,10 +54,10 @@ int do_work(IHardwareInterface& hwif)
     tcp_client_initialise(hwif.raw_if(), WIFI_SSID, WIFI_PASSWORD);
 
     run_tcp_client_test(&processor, TEST_TCP_SERVER_IP, TCP_PORT, "GET /codes HTTP/1.1\r\nHost: test.com\r\nAccept: */*\r\n\r\n", buffer, BUFFER_LENGTH);
-    printf("--Restule 1--\n%s\n+++", buffer);
+    printf("--Restule 1--\n%s\n+++\n", buffer);
 
-    run_tcp_client_test(&processor, TEST_TCP_SERVER_IP, TCP_PORT, "GET /codes HTTP/1.1\r\nHost: test.com\r\nAccept: */*\r\n\r\n", buffer, BUFFER_LENGTH);
-    printf("--Restule 2--\n%s\n+++", buffer);
+//    run_tcp_client_test(&processor, TEST_TCP_SERVER_IP, TCP_PORT, "GET /codes HTTP/1.1\r\nHost: test.com\r\nAccept: */*\r\n\r\n", buffer, BUFFER_LENGTH);
+//    printf("--Restule 2--\n%s\n+++\n", buffer);
 
     tcp_client_uninit();
     return 0;
