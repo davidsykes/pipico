87f1e4bad19f631f10d402753671b3cc44808bd9
David Sykes <david@sykes.org.uk>
2024-07-17 07:53:22 +0100

    Reformat the trace output

--- hotspot/Tests/pico_scope/TraceDataFormatterTests.cpp
+++ hotspot/Tests/pico_scope/TraceDataFormatterTests.cpp
@@ -1,16 +1,16 @@
 #include "TraceDataFormatterTests.h"
 #include "../../pico_scope/pico_scope.h"
 
-const char* prefix = "{\"type\":\"trace\",\"data\":\"";
+const char* prefix = "{\"InitialValue\":11,\"values\":";
 
 std::string FormatTraceDataAndRemovePrefix(const PicoScopeTrace& trace);
 
 static void TheDataBeginsWithAStandardHeader()
 {
 	PicoScopeTrace trace;
-	trace.Reset(1);
-	trace.AddChange(10);
-	trace.AddChange(20);
+	trace.Reset(11, 42);
+	trace.AddChange(21, 10);
+	trace.AddChange(34, 20);
 
 	TraceDataFormatter formatter;
 
@@ -23,30 +23,13 @@ static void TheDataBeginsWithAStandardHeader()
 static void ASimpleTraceCanBeFormatted()
 {
 	PicoScopeTrace trace;
-	trace.Reset(1);
-	trace.AddChange(10);
-	trace.AddChange(20);
+	trace.Reset(11, 42);
+	trace.AddChange(21, 10);
+	trace.AddChange(34, 20);
 
 	std::string data = FormatTraceDataAndRemovePrefix(trace);
 
-	AssertEqual("78563412", data.substr(0, 8));
-	AssertEqual("02000000", data.substr(8, 8));
-	AssertEqual("14000000", data.substr(16, 8));
-	AssertEqual("000000001", data.substr(24, 9));
-	AssertEqual("0a0000000", data.substr(33, 9));
-	AssertEqual("140000001", data.substr(42, 9));
-	AssertEqual("\"}", data.substr(51, 2));
-}
-
-static void ATraceWithNoValuesCanBeFormatted()
-{
-	PicoScopeTrace trace;
-	trace.Reset(1);
-
-	const char* expected = "7856341200000000\"}";
-	std::string data = FormatTraceDataAndRemovePrefix(trace);
-
-	AssertEqual(expected, data);
+	AssertEqual("{{42,0},{21,10},{34,20}}}", data);
 }
 
 
@@ -54,7 +37,6 @@ void TraceDataFormatterTests::RunTests()
 {
 	TheDataBeginsWithAStandardHeader();
 	ASimpleTraceCanBeFormatted();
-	ATraceWithNoValuesCanBeFormatted();
 }
 void TraceDataFormatterTests::CleanUpAfterTests()
 {
--- hotspot/pico_scope/trace_data_formatter.cpp
+++ hotspot/pico_scope/trace_data_formatter.cpp
@@ -1,61 +1,24 @@
 #include "pico_scope.h"
 #include <sstream>
 
-#include <string>
-#include <iomanip>
 
-const std::string prefix = "{\"type\":\"trace\",\"data\":\"";
-
-void output_int(std::stringstream& s, int i);
-void output_word(std::stringstream& s, int i);
-void output_byte(std::stringstream& s, int i);
 
 std::string TraceDataFormatter::FormatTraceData(const PicoScopeTrace& trace)
 {
     std::stringstream s;
-    s << prefix;
-    s << std::hex;
-    output_int(s, 0x12345678);
+    s << "{\"InitialValue\":";
+    s << trace.initial_value;
+    s << ",\"values\":{";
 
-    size_t sample_count = trace.changes.size();
-    output_int(s, sample_count);
-
-    if (sample_count > 0)
+    for (int v = 0; v < trace.values.size(); ++v)
     {
-        int trace_length_us = trace.changes[sample_count - 1];
-        output_int(s, trace_length_us);
-
-        int value = trace.start_value;
-        output_int(s, 0);
-        s << value;
-
-        for (int i = 0; i < sample_count; ++i)
+        if (v > 0)
         {
-            unsigned int change = trace.changes[i];
-            value = 1 - value;
-            output_int(s, change);
-            s << value;
+            s << ",";
         }
+        s << "{" << trace.values[v] << "," << trace.value_times[v] << "}";
     }
 
-    s << "\"}";
+    s << "}}";
     return s.str();
-}
-
-void output_int(std::stringstream& s, int i)
-{
-    output_word(s, i);
-    output_word(s, i >> 16);
-}
-
-void output_word(std::stringstream& s, int i)
-{
-    output_byte(s, i);
-    output_byte(s, i >> 8);
-}
-
-void output_byte(std::stringstream& s, int i)
-{
-    s << ((i >> 4) & 15);
-    s << (i & 15);
 }
