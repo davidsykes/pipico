3d14603097015594bb1cf41aba904f76d85ceddd
David Sykes <david@sykes.org.uk>
2024-07-30 07:00:47 +0100

    A trace can be plotted

--- picoscope/traceviewer/src/trace_plotter.py
+++ picoscope/traceviewer/src/trace_plotter.py
@@ -9,16 +9,17 @@ class TracePlotter:
         trace_count = len(list_of_traces_to_plot)
         current_trace = 0
         for trace_number in list_of_traces_to_plot:
-            trace_data = self._trace_directory.get_trace_data_by_number(trace_number)
+            trace_details = self._trace_directory.get_trace_data_by_number(trace_number)
+            trace_data = trace_details['rawScopeTraceData']
             sample_count = len(trace_data)
             x = []
             y = []
-            previous_value = trace_data[0]['value']
-            for s in trace_data:
-                new_value = s['value'] + current_trace * 300
-                x.append(s['time'])
+            previous_value = trace_data['initialValue']
+            for s in trace_data['values']:
+                new_value = s[1] + current_trace * 300
+                x.append(s[0])
                 y.append(previous_value)
-                x.append(s['time'])
+                x.append(s[0])
                 y.append(new_value)
                 previous_value = new_value
             ax.plot(x, y)
