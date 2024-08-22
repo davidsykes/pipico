eafa9bfdb2552baf9a93334215df531ed3dd3798
David Sykes <david@sykes.org.uk>
2024-08-22 07:11:37 +0100

    Programming by wishful thinking

--- hotspot/main.cpp
+++ hotspot/main.cpp
@@ -35,15 +35,29 @@ int main()
    {
       connector.ConnectToWiFiDirect(hw_if, WIFI_SSID, WIFI_PASSWORD);
 
+      MessageLogger messageLogger;
+
+      #define SCOPE_API_SERVER_IP "192.168.1.87"
+      #define SCOPE_API_PORT 5000
+      TcpResponseAnalyser& tcp_analyser;
+      RestHandler restHandler(hw_if, tcp_analyser, SCOPE_API_SERVER_IP, SCOPE_API_PORT);
+
+      IrCodesCollection irCodesCollection;
+      IrCodesFetcher.FetchCodes(restHandler, irCodesCollection);
+
       CodesDisplayRequestHandler codesDisplayRequestHandler;
       CodesRecordRequestHandler codesRecordRequestHandler;
-      HttpServerHomeHandler homeHandler(codesDisplayRequestHandler, codesRecordRequestHandler);
+      LogDisplayWidget LogDisplayWidget(messageLogger);
+      HttpServerHomePage homeHandler(codesDisplayRequestHandler, codesRecordRequestHandler, LogDisplayWidget);
+
       PicoScopeConfiguration ir_scope_configuration(6);
       PicoScopeRecordAndPost picoScopeRecordAndPost(hw_if, ir_scope_configuration);
       HttpServerRecordHandler recordHandler(picoScopeRecordAndPost);
+
       HttpRequestRouter httpRequestRouter(homeHandler, recordHandler);
       HttpResponsePackager httpResponsePackager(httpRequestRouter);
       main_pico_tcp_server(&httpResponsePackager);
+
       run_ir_main(hw_if, connector);
    }
 
