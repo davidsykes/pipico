973a443c7231cb2fc086b2f67b06cb95e56252df
David Sykes <david@sykes.org.uk>
2024-09-04 06:21:02 +0100

    Copy LogDisplayWidget code into new transmitter

--- IR/transmitter/cpp/IrXmitterTests/IrXmitterTests.cpp
+++ IR/transmitter/cpp/IrXmitterTests/IrXmitterTests.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "api\http_request_router_tests.h"
 #include "tcp_server/http_response_packager_tests.h"
+#include "tcp_server/widgets/logs_display_widget_tests.h"
 #include "tools\MessageLoggerTests.h"
 
 int main()
@@ -8,5 +9,6 @@ int main()
     HttpRequestRouterTests().RunTests();
     HttpResponsePackagerTests().RunTests();
     MessageLoggerTests().RunTests();
+    LogsDisplayWidgetTests().RunTests();
     std::cout << "Tests Complete\n";
 }
