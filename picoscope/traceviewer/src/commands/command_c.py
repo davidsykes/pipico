9ff0cc910b90e1c73d8416a5b3e68376259f467c
David Sykes <david@sykes.org.uk>
2024-09-22 07:25:26 +0100

    Tracevier converting a trace into a code for the repository

--- picoscope/traceviewer/src/commands/command_c.py
+++ picoscope/traceviewer/src/commands/command_c.py
@@ -3,5 +3,21 @@ class CommandC:
         self._trace_directory = trace_directory
 
     def process_command(self, arguments=None):
-        print('argu', arguments)
         trace_details = self._trace_directory.get_trace_data_by_number(arguments[0])
+        tracecount = trace_details['scopeTraceDetails']['tracecount']
+        print('char CodeName[] = "";')
+        print ('int CodeSize = ' + str(tracecount) + ';')
+        times = 'int CodeTimes[] = { '
+        values = 'char CodeValues[] = { '
+
+        for index, x in enumerate(trace_details['rawScopeTraceData']['values']):
+            if (index > 0):
+                times = times + ', '
+                values = values + ', '
+            times = times + str(x[0])
+            value = '1' if x[1] == 0 else '0'
+            values = values + value
+        times = times + ' };'
+        values = values + ' };'
+        print(times)
+        print(values)
