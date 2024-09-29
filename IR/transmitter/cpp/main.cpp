ef87147b12e2542bc9770d4cb3d6384063627aa6
David Sykes <david@sykes.org.uk>
2024-09-29 06:04:27 +0100

    Implement sequence page on the pico

--- IR/transmitter/cpp/main.cpp
+++ IR/transmitter/cpp/main.cpp
@@ -6,12 +6,13 @@
 #include "tcp_server/http_response_packager.h"
 #include "tcp_server/pico_tcp_server.h"
 #include "api/api_actions/home_display_action.h"
+#include "api/api_actions/raw_display_action.h"
 #include "api/api_actions/log_display_action.h"
 #include "api/api_actions/codes_display_action.h"
 #include "api/api_actions/play_ir_action.h"
 #include "api/formatters/code_display_formatter.h"
 #include "pico_scope/pico_scope_main.h"
-#include "transmit/ir_signal_sender.h"
+#include "transmit/ir_code_sender.h"
 
 #define WIFI_SSID "a907"
 #define WIFI_PASSWORD "?thisistheWIFIyouhavebeenlookingfor1398"
@@ -41,16 +42,19 @@ int main()
       IGPIOOutputPin& outputPin = _outputPin;
       MessageLogger messageLogger;
       IrCodesRepository irCodesRepository;
+      CodeSequenceRepository codeSequenceRepository;
+      ButtonFormatter buttonFormatter;
+      HomeDisplayAction homeDisplayAction(codeSequenceRepository, buttonFormatter);
 
-      LogDisplayAction logDisplayAction(messageLogger);
       CodeDisplayFormatter codeDisplayFormatter;
       CodesDisplayAction codesDisplayAction(irCodesRepository, codeDisplayFormatter);
-      HomeDisplayAction homeDisplayAction(codesDisplayAction, logDisplayAction);
+      LogDisplayAction logDisplayAction(messageLogger);
+      RawDisplayAction rawDisplayAction(codesDisplayAction, logDisplayAction);
 
-      IrSignalSender irSignalSender(outputPin);
-      PlayIrAction playIrAction(irSignalSender, irCodesRepository);
+      IrCodeSender irCodeSender(outputPin);
+      PlayIrAction playIrAction(irCodeSender, irCodesRepository, homeDisplayAction);
 
-      HttpRequestRouter httpRequestRouter(homeDisplayAction, playIrAction);
+      HttpRequestRouter httpRequestRouter(homeDisplayAction, rawDisplayAction, playIrAction);
       HttpResponsePackager httpResponsePackager(httpRequestRouter);
       hw_if.set_led(true);
       main_pico_tcp_server(&httpResponsePackager);
