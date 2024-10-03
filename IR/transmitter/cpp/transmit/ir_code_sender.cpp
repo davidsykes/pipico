7ed0da4fa0a428855c9dc12d11dc8973cc0f45d6
David Sykes <david@sykes.org.uk>
2024-10-03 06:35:07 +0100

    Remove some debugging

--- IR/transmitter/cpp/transmit/ir_code_sender.cpp
+++ IR/transmitter/cpp/transmit/ir_code_sender.cpp
@@ -3,21 +3,11 @@
 
 void IrCodeSender::SendCode(const IrCode& code)
 {
-	std::vector<int> times;
-	times.reserve(code.Values.size());
-
 	uint64_t i = 0;
 	uint64_t start_time = pin.SetValueAt(code.Values[i], 0);
 
-	times.push_back(start_time);
-
 	while (++i < code.Count)
 	{
-		times.push_back(pin.SetValueAt(code.Values[i], start_time + code.Times[i]));
-	}
-
-	for (int x = 0 ; x < times.size() ; ++x)
-	{
-		printf("%d | %d | %d\n", code.Times[x], times[x], times[x] - times[0]);
+		pin.SetValueAt(code.Values[i], start_time + code.Times[i]);
 	}
 };
