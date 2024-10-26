a735747670b95ae489b9004cb3e9628a20784353
David Sykes <david@sykes.org.uk>
2024-10-26 08:03:13 +0100

    Remove some old picoscope projects

--- picoscope/tracesender/mock/exit_mock_system_exception.py
+++
@@ -1,2 +0,0 @@
-class ExitMockSystemException(Exception):
-	pass
--- picoscope/tracesender/mock/machine.py
+++
@@ -1,9 +0,0 @@
-class Pin:
-    OUT = 1
-    def __init__(self, name, direction):
-        self.name = name
-    def value(self, val):
-        if self.name == 'LED':
-            print('LED ', 'on' if val == 1 else 'off')
-        else:
-            raise Exception(''.join(['Invalid pin "', self.name, '"']))
