1f0aa27f72c7ae55352f0afa13c9aa5e38dfd0a1
David Sykes <david@sykes.org.uk>
2024-11-09 07:37:18 +0000

    The wifi connector library connects with existing credentials

--- temperature/wificonnector/src/wificonnector.cpp
+++ temperature/wificonnector/src/wificonnector.cpp
@@ -1,66 +1,66 @@
 #include "wificonnector.h"
-// #include "hotspot_request_router.h"
-// #include "flash/flash_hardware.h"
-// #include "flash/flash_writer.h"
-// #include "flash/flash_parameters_reader.h"
+#include "hotspot_request_router.h"
+#include "flash/flash_hardware.h"
+#include "flash/flash_writer.h"
+#include "flash/flash_parameters_reader.h"
 #include "hw_if/wifi_hardware_interface.h"
-// #include "input_form/ssid_password_input_form_renderer.h"
-// #include "input_form/html_renderer.h"
-// #include "input_form/input_form_renderer.h"
-// #include "input_form/percent_decoder.h"
-// #include "input_form/ssid_password_input_form_renderer.h"
-// #include "input_form/ssid_password_submitter.h"
-// #include "tcpserver/http_response_packager.h"
-// #include "tcpserver/pico_tcp_server.h"
+#include "input_form/ssid_password_input_form_renderer.h"
+#include "input_form/html_renderer.h"
+#include "input_form/input_form_renderer.h"
+#include "input_form/percent_decoder.h"
+#include "input_form/ssid_password_input_form_renderer.h"
+#include "input_form/ssid_password_submitter.h"
+#include "tcpserver/http_response_packager.h"
+#include "tcpserver/pico_tcp_server.h"
 
  void WiFiConnector::ConnectToWiFi(const char* input_form_hotspot_name, const char* password)
  {
     WiFiHardwareInterface _hw_if;
     IWiFiHardwareInterface& hw_if(_hw_if);
     hw_if.initialise_pico_stdio(true);
-//     FlashHardware flashHardware;
+    FlashHardware flashHardware;
     
-//     FlashParametersReader flashParametersReader_(flashHardware);
-//     IFlashParametersReader& flashParametersReader(flashParametersReader_);
-//     if (!flashParametersReader.ReadParameters())
-//     {
-//       printf("Flash parameters not found\n");
-//     }
-//     else if (!flashParametersReader.ContainsParameter("ssid"))
-//     {
-//       printf("ssid not found\n");
-//     }
-//     else if (!flashParametersReader.ContainsParameter("password"))
-//     {
-//       printf("password not found\n");
-//     }
-//     else if (hw_if.initialise_wifi_sta(
-//       flashParametersReader.GetParameter("ssid").c_str(),
-//       flashParametersReader.GetParameter("password").c_str()) != 0)
-//     {
-//       printf("Connection to wifi failed\n");
-//     }
-//     else
-//     {
-//          return;
-//     }
+    FlashParametersReader flashParametersReader_(flashHardware);
+    IFlashParametersReader& flashParametersReader(flashParametersReader_);
+    if (!flashParametersReader.ReadParameters())
+    {
+      printf("Flash parameters not found\n");
+    }
+    else if (!flashParametersReader.ContainsParameter("ssid"))
+    {
+      printf("ssid not found\n");
+    }
+    else if (!flashParametersReader.ContainsParameter("password"))
+    {
+      printf("password not found\n");
+    }
+    else if (hw_if.initialise_wifi_sta(
+      flashParametersReader.GetParameter("ssid").c_str(),
+      flashParametersReader.GetParameter("password").c_str()) != 0)
+    {
+      printf("Connection to wifi failed\n");
+    }
+    else
+    {
+         return;
+    }
 
-//    hw_if.initialise_wifi_ap(input_form_hotspot_name, password);
+   hw_if.initialise_wifi_ap(input_form_hotspot_name, password);
    
-//    HtmlRenderer htmlRenderer;
-//    InputFormRenderer inputFormRenderer;
-//    SSIDPasswordInputFormRenderer ssidPasswordInputFormRenderer("PicoIR", htmlRenderer, inputFormRenderer);
-//    FlashWriter flashWriter(flashHardware);
-//    PercentDecoder percentDecoder;
-//    SSIDPasswordSubmitter ssidPasswordSubmitter(flashWriter, percentDecoder);
-//    HotSpotRequestRouter httpRequestRouter(ssidPasswordInputFormRenderer, ssidPasswordSubmitter);
-//    HttpResponsePackager httpResponsePackager(httpRequestRouter);
-//    run_tcp_server(&httpResponsePackager);
+   HtmlRenderer htmlRenderer;
+   InputFormRenderer inputFormRenderer;
+   SSIDPasswordInputFormRenderer ssidPasswordInputFormRenderer("PicoIR", htmlRenderer, inputFormRenderer);
+   FlashWriter flashWriter(flashHardware);
+   PercentDecoder percentDecoder;
+   SSIDPasswordSubmitter ssidPasswordSubmitter(flashWriter, percentDecoder);
+   HotSpotRequestRouter httpRequestRouter(ssidPasswordInputFormRenderer, ssidPasswordSubmitter);
+   HttpResponsePackager httpResponsePackager(httpRequestRouter);
+   run_tcp_server(&httpResponsePackager);
 }
 
 void WiFiConnector::ConnectToWiFiDirect(const char* ssid, const char* password)
 {
-//   WiFiHardwareInterface _hw_if;
-//   IWiFiHardwareInterface& hw_if(_hw_if);
-//   hw_if.initialise_wifi_sta(ssid, password);
+  WiFiHardwareInterface _hw_if;
+  IWiFiHardwareInterface& hw_if(_hw_if);
+  hw_if.initialise_wifi_sta(ssid, password);
 }
