473c32a02337456c143845f81694785fd7c4cfb1
David Sykes <david@sykes.org.uk>
2024-11-09 06:17:37 +0000

    Example makefile builds but doesnt produce what I expect

--- temperature/src/main.cpp
+++ temperature/src/main.cpp
@@ -7,10 +7,10 @@
 #include "pico/stdlib.h"
 
 int main() {
-#ifndef PICO_DEFAULT_LED_PIN
+#ifndef CYW43_WL_GPIO_LED_PIN
 #warning blink example requires a board with a regular LED
 #else
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    const uint LED_PIN = CYW43_WL_GPIO_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true) {
