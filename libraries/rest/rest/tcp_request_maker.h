09c631bbb15243f90e1a37d0179d59b71446209b
David Sykes <david@sykes.org.uk>
2025-02-03 08:00:56 +0000

    Setting up a rest library

---
+++ libraries/rest/rest/tcp_request_maker.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <string>
+#include "../hw_if/ihardware_interface.h"
+#include "tcp_response_analyser.h"
+#include "tcp_request_error_logger.h"
+
+class ITcpRequestMaker
+{
+public:
+    virtual int MakeRequest(const std::string& request, std::string& response) = 0;
+    virtual ~ITcpRequestMaker() {}
+};
+
+
+class TcpRequestMaker : public ITcpRequestMaker
+{
+    std::string server;
+    unsigned int port;
+    IHardwareInterface& hw_if;
+    ITcpResponseAnalyser& tcpResponseAnalyser;
+    ITcpRequestErrorLogger& tcpRequestErrorLogger;
+
+    virtual int MakeRequest(const std::string& request, std::string& response);
+
+    int ExtractStatusAndBody(const std::string& server_response, std::string& response);
+    int MakeServerErrorResponse(int tcp_request_result, std::string& response);
+public:
+    TcpRequestMaker(
+        const std::string& server,
+        unsigned int port,
+        IHardwareInterface& hw_if,
+        ITcpResponseAnalyser& tcpResponseAnalyser,
+        ITcpRequestErrorLogger& tcpRequestErrorLogger) :
+        server(server),
+        port(port),
+        hw_if(hw_if),
+        tcpResponseAnalyser(tcpResponseAnalyser),
+        tcpRequestErrorLogger(tcpRequestErrorLogger){}
+};
--- web_button/hw_if/readme.txt
+++
@@ -1,9 +0,0 @@
-This is not an actual library, but a first step in creating a module that can be reused
-
-Copy the folder into the project directory
-
-
-To create an interface object
-
-#include "hw_if/pico_hardware_interface.h"
-IHardwareInterface* hw_if = new PicoHardwareInterface();
